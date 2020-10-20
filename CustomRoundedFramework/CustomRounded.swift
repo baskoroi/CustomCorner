580a9717c000bc4d3f753f2db80108306599e055
Baskoro Indrayana <baskoroindrayana@gmail.com>
2020-10-20 11:18:36 +0700

    Initial commit for framework

---
+++ CustomRoundedFramework/CustomRounded.swift
@@ -0,0 +1,30 @@
+//
+//  CustomRounded.swift
+//  BeautyShape
+//
+//  Created by Baskoro Indrayana on 10/20/20.
+//  Copyright © 2020 Apple Developer Academy. All rights reserved.
+//
+
+import UIKit
+
+public class CustomRounded {
+    
+    public static func allRoundedCorners(view: UIView) {
+        view.layer.cornerRadius = 5.0
+        view.layer.masksToBounds = true
+    }
+    
+    public static func roundedTopRightTopLeft(view: UIView) {
+        view.clipsToBounds = true
+        view.layer.cornerRadius = 10
+        view.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner] // Top right corner, Top left corner respectively
+    }
+    
+    public static func roundedBottomRightBottomLeft(view: UIView) {
+        view.clipsToBounds = true
+        view.layer.cornerRadius = 10
+        view.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner] // Bottom right corner, Bottom left corner respectively
+    }
+    
+}
